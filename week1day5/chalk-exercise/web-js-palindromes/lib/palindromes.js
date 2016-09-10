9c4f928a8e641f1a8a973cd1237fc687af33ba4a
treeforever <treeforever7@gmail.com>
2016-09-10 00:35:13 -0400

    file management

--- week1day5/chalk-exercise/web-js-palindromes/lib/palindromes.js
+++
@@ -1,7 +0,0 @@
-function isPalindrome(s) {
-  var original = s.replace(/ /g, "");
-  stringReverse = original.split("").reverse().join("");
-  return original == stringReverse;
-}
-
-module.exports = isPalindrome;
---
+++ week1day5/test.js
@@ -0,0 +1,6 @@
+var arr = [{a: 100, b:23}, {c:454, d: 33}, {f: 31, r:78}, 302,777,-11,0];
+arr.sort(function(a, b){
+  return 0;
+})
+
+console.log(arr);
