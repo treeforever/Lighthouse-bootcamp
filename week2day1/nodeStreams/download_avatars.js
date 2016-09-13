39aa81040e231443d26b5e9ec29853baf4d40e11
treeforever <treeforever7@gmail.com>
2016-09-13 23:34:38 +0000

    moved files

---
+++ week2day1/nodeStreams/download_avatars.js
@@ -0,0 +1,43 @@
+var request = require("request");
+var fs = require('fs');
+require('dotenv').config();
+var token = process.env.token;
+var download = require("./download_function");
+var downloadAllAvatars = download.downloadAllAvatars;
+
+function githubRequest(endpoint, callback) {
+  var requestData = {
+    url: `http://api.github.com${endpoint}`,
+    auth: {
+      bearer: token
+    },
+    headers: {
+      'User-Agent': "request"
+    }
+  };
+  request.get(requestData, callback);
+}
+
+function getRepoContributors(repoOwner, repoName, callback) {
+  githubRequest(`/repos/${repoOwner}/${repoName}/contributors`, (err, response, body) => {
+
+    var data = JSON.parse(body);
+    if (data.message === 'Not Found') {
+      throw new Error('nonexisting owner or repo provided');
+    } else if (data.message === 'Bad credentials') {
+      throw new Error('Bad credentials');
+    } else {
+      callback(data);
+    }
+  });
+}
+
+if (!fs.existsSync('./.env')){
+  throw new Error ("Don't have .env file");
+} else {
+  if (process.argv.length !== 4) {
+    throw new Error ("incorrect number of arguments");
+  } else {
+    getRepoContributors(process.argv[2], process.argv[3], downloadAllAvatars);
+  }
+}
