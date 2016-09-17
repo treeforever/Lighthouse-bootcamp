56a692ee3371b9c178a46630a3f3cc86db09051e
treeforever <treeforever7@gmail.com>
2016-09-17 22:32:45 +0000

    fixed non-existing urls

--- week2day3/TinyApp/express_server.js
+++ week2day3/TinyApp/express_server.js
@@ -11,11 +11,13 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://127.0.0.1:27017/url_shortener";
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
-// let urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
+
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     throw err;
@@ -23,19 +25,8 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
 
   function getLongURL(db, shortURL, cb) {
     let query = { "shortURL": shortURL };
-    db.collection("urls").findOne(query, (err, result) => {
-      if (err) {
-        cb(err);
-      }
-      cb(result.longURL);
-    });
+    db.collection("urls").findOne(query, cb);
   }
-
-  // function search (cb) {
-  //   db.collection("urls").find().toArray((err, result) => {
-  //     cb(result);
-  //   });
-  // }
 
   app.get("/hello", (req, res) => {
     res.end("<html><body>Hello <b>World</b></body></html>\n");
@@ -49,6 +40,7 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
     db.collection("urls").find().toArray((err, results) => {
       res.render("urls_index", {
         database: results
+        // messages: req.flash('error')
       });
     });
   });
@@ -72,24 +64,39 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   app.post("/urls/create", (req, res) => {
     let randomStr = generateRandomString();
     let longURL = req.body.longURL;
-    let doc = {
-      shortURL: randomStr,
-      longURL: longURL
-    };
 
-    db.collection("urls").insert(doc, (err, results) => {
-      res.redirect(`http://localhost:8080/urls/${randomStr}`);
-    });
+    if (longURL.substring(0, 7) !== "http://") {
+      res.redirect("/urls/new/");
+    } else {
+      let doc = {
+        shortURL: randomStr,
+        longURL: longURL
+      };
+
+      db.collection("urls").insert(doc, (err, results) => {
+        res.redirect(`http://localhost:8080/urls/${randomStr}`);
+      });
+    }
   });
 
   app.get("/urls/:id", (req, res) => {
     let shortURL = req.params.id;
-    getLongURL(db, shortURL, (longURL) => {
-      res.render("urls_show", {
-        shortURL: shortURL,
-        longURL: longURL
-      });
+
+    getLongURL(db, shortURL, (err, result) => {
+      if (err || result == null) {
+        res.redirect("/urls");
+      } else {
+        res.render("urls_show", {
+          shortURL: shortURL,
+          longURL: result.longURL
+        });
+      }
     });
+  });
+
+  app.get("/flash", (req, res) => {
+    req.flash("error", "Url does not exist!");
+    res.redirect('/urls');
   });
 
   app.put("/urls/:id", (req, res) => {
@@ -103,16 +110,20 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
     db.collection("urls").deleteOne(
       {shortURL: shortU},
       res.redirect('/urls'));
-  });
+    });
 
-  app.get("/u/:shortURL", (req, res) => {
-    let shortURL = req.params.shortURL;
-    getLongURL(db, shortURL, (longURL) => {
-      res.redirect(longURL);
+    app.get("/u/:shortURL", (req, res) => {
+      let shortURL = req.params.shortURL;
+      getLongURL(db, shortURL, (err, result) => {
+        if (err || result == null) {
+          res.redirect('/urls');
+        } else {
+          res.redirect(undefined, result.longURL);
+        }
+      });
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}!`);
     });
   });
-
-  app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
-  });
-});
